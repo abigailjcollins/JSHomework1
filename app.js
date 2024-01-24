e09035af9d0d185b715473f6b8b7abd6fd450c54
Abigail Collins <abigailjcollins@gmail.com>
2024-01-24 17:54:35 -0500

    JS HW on functions

---
+++ app.js
@@ -0,0 +1,27 @@
+var a = 10;
+var b = 20;
+function addTwo(num1, num2) {
+  return num1 + num2;
+}
+console.log(addTwo(a,b));
+
+
+
+
+function divideTwo(num1, num2) {
+  return num1 / num2;
+}
+console.log(divideTwo(a,b));
+
+
+
+function multiplyTwo(num1, num2) {
+  return num1 * num2;
+}
+console.log(multiplyTwo(a,b));
+
+
+function subtractTwo(num1, num2) {
+  return num1 - num2;
+}
+console.log(subtractTwo(a,b));
